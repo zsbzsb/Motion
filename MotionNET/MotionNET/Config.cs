15d45ca87803d522598d5da9a246863c40272547
zekiahepic <73035340+Zekiah-A@users.noreply.github.com>
2023-03-25 23:08:56 +0000

    Make projects .net 6 compatible

--- MotionNET/MotionNET/Config.cs
+++ MotionNET/MotionNET/Config.cs
@@ -4,12 +4,7 @@ namespace MotionNET
 {
     internal static class Config
     {
-        #if DEBUG
-            internal const string MotionDll = "Motion-d";
-            internal const CallingConvention MotionCall = CallingConvention.Cdecl;
-        #else
-            internal const string Motion_DLL = "Motion";
-            internal const CallingConvention Motion_Call = CallingConvention.Cdecl;
-        #endif
+        internal const string Motion_DLL = "Motion";
+        internal const CallingConvention Motion_Call = CallingConvention.Cdecl;
     }
 }
